ebc17f546402973a4fdf03e5b3d0d2482d97b930
gaikwad-siddharth <sidgaikwad0904@gmail.com>
2026-06-16 11:22:47 +0530

    changes in rest cotroller

--- src/main/java/in/siddharth/rest/UserResrController.java
+++ src/main/java/in/siddharth/rest/UserResrController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import in.siddharth.dto.CityDto;
 import in.siddharth.dto.CountryDto;
 import in.siddharth.dto.StateDto;
+import in.siddharth.dto.UserDto;
 import in.siddharth.service.UserService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 public class UserResrController {
@@ -36,6 +39,25 @@ public class UserResrController {
 	public ResponseEntity<List<CityDto>> getCities(@PathVariable Integer stateId){
 		List<CityDto> states = userService.getCities(stateId);
 		return new ResponseEntity<>(states, HttpStatus.OK);
+	}
+	
+	public ResponseEntity<String> registerUser(@RequestBody UserDto userDto){
+		boolean register = userService.register(userDto);
+		if(register) {
+			return new ResponseEntity<>("Regsitration Sucess", HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>("Regsitration Failed", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	public ResponseEntity<?> login(@RequestBody UserDto userDto){
+		UserDto user = userService.login(userDto.getEmail(), userDto.getPwd());
+		if(user != null)  {
+			return ResponseEntity.ok(user);
+		}else {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+								.body("Invalid Credentials");
+		}
 	}
 
 }
